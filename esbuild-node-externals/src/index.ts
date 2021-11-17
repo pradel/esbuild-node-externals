1c2629984a6ed536790c88152bbfd7fdb2e363d6
Pradel Léo <pradel.leo@gmail.com>
2021-11-17 18:47:36 +0100

    add comment

--- esbuild-node-externals/src/index.ts
+++ esbuild-node-externals/src/index.ts
@@ -16,7 +16,7 @@ export const nodeExternalsPlugin = (paramsOptions: Options = {}): Plugin => {
     devDependencies: true,
     peerDependencies: true,
     optionalDependencies: true,
-    allowList: [],
+    allowList: [] as string[],
     ...paramsOptions,
     packagePath:
       paramsOptions.packagePath && typeof paramsOptions.packagePath === 'string'
@@ -40,8 +40,9 @@ export const nodeExternalsPlugin = (paramsOptions: Options = {}): Plugin => {
     setup(build) {
       // On every module resolved, we check if the module name should be an external
       build.onResolve({ namespace: 'file', filter: /.*/ }, (args) => {
+        // To allow allowList to target sub imports
         if (options.allowList.includes(args.path)) {
-          return null
+          return null;
         }
 
         // To allow sub imports from packages we take only the first path to deduct the name
