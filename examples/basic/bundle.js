9c865925874cb960163bda777ae030a13af962ff
Anton Golub <antongolub@antongolub.com>
2024-09-26 14:34:59 +0300

    feat: add esbuild 0.24 to allowed range, extend integration test (#65)

--- examples/basic/bundle.js
+++ examples/basic/bundle.js
@@ -1,10 +1,20 @@
 const esbuild = require('esbuild');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
 
-esbuild.build({
-  entryPoints: ['src/index.js'],
-  bundle: true,
-  platform: 'node',
-  outfile: 'dist/index.js',
-  plugins: [nodeExternalsPlugin()],
-});
+(async () => {
+  try {
+    await esbuild.build({
+      entryPoints: ['src/index.js'],
+      bundle: true,
+      platform: 'node',
+      outfile: 'dist/index.js',
+      plugins: [nodeExternalsPlugin()],
+    });
+    console.log(`Built with ${esbuild.version}`)
+    process.exit(0);
+  }
+  catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})()
