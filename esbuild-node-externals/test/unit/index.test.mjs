a7e71a6f63cd7f9be9df6e9b8836bbfd771e8cb2
Léo Pradel <pradel.leo@gmail.com>
2026-08-04 14:34:49 +0200

    chore: upgrade deps

--- esbuild-node-externals/test/unit/index.test.mjs
+++ esbuild-node-externals/test/unit/index.test.mjs
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url';
 
 import { build } from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
-import { describe, it } from 'vitest';
+import { describe, it } from 'vite-plus/test';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
